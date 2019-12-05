b294435a796a749e1959559393047d2db5f391a9
Tina Wang <tinary@users.noreply.github.com>
2019-12-04 23:09:42 -0500

    Update dayOffset_1line.js

--- Weekly Problems/dayOffset_1line.js
+++ Weekly Problems/dayOffset_1line.js
@@ -11,9 +11,7 @@ Example:
 
 var week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-function dayOffset(s, n) {
-    return week[(week.indexOf(s) + (n + 364)) % 7];
-}
+const dayOffset = (s, n) => {return week[(week.indexOf(s) + (n + 364)) % 7];}
 
 
 console.log(dayOffset('Wednesday', 4));
