3d3a170e29392062af01ee3af85736b4a1a50561
Tina Wang <tinary@users.noreply.github.com>
2019-12-04 20:39:57 -0500

    add dayOffset one line code

---
+++ Weekly Problems/dayOffset_oneline.js
@@ -0,0 +1,24 @@
+/*
+Write a function that takes the name of a day (e.g "Monday", "Tuesday") and an integer offset, 
+and returns the name of the day which would be the original + offset.
+
+Extra credit: do it in 1 line!
+
+Example:
+> dayOffset("Wednesday", 4)
+> "Sunday"
+*/
+
+var week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+function dayOffset(s, n) {
+    return week[(week.indexOf(s) + (n + 364)) % 7];
+}
+
+console.log(dayOffset('Wednesday', 4));
+console.log(dayOffset('Wednesday', -5));
+console.log(dayOffset('Tuesday', 14));
+console.log(dayOffset('Monday', -15));
+console.log(dayOffset('Monday', 0));
+console.log(dayOffset('Monday', -1));
+console.log(dayOffset('Sunday', -7));
